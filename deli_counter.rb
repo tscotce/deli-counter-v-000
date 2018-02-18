bfddb9627492ac6ab1f04a7fd29ddef4379e5c0f
Tatyana Scotce <tscotce@gmail.com>
2018-02-18 20:31:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 katz_deli = []
 
-def line(katz_deli, name) 
+def line(katz_deli) 
   if katz_deli.length == 0 
     puts "The line is currently empty."
   elsif katz_deli.length > 0 
-    puts "The line is currently: #{katz_deli[0].index+1}. #{katz_deli[0]} #{katz_deli[1].index+2}. #{katz_deli[1]} #{katz_deli[2].index+3}. #{katz_deli[2]}"
+    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
   end
 end
 
