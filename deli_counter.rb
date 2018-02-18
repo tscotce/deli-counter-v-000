2e1b5a3044ecece893bc8246066b029a4caed6f8
Tatyana Scotce <tscotce@gmail.com>
2018-02-18 20:56:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,3 +18,11 @@ def take_a_number(katz_deli, name)
     puts "Welcome, #{name}. You are number #{number} in line."
   end
 end
+
+def now_serving(katz_deli, name)
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served!"
+  elsif katz_deli.length > 0 
+    puts
+  end
+end
