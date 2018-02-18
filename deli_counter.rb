07d81a67d103f6e67b87204429bb6619748fc2dc
Tatyana Scotce <tscotce@gmail.com>
2018-02-18 21:00:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def take_a_number(katz_deli, name)
   end
 end
 
-def now_serving(katz_deli, name)
+def now_serving(katz_deli)
   if katz_deli.length == 0 
     puts "There is nobody waiting to be served!"
   elsif katz_deli.length > 0 
