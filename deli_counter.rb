903030937507709daaf1f17b870a401acada419e
Tatyana Scotce <tscotce@gmail.com>
2018-02-18 21:21:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   elsif katz_deli.length > 0 
-    katz_deli.each do |name|
+    katz_deli.each_with_index do |name, index|
       helper_array = [ ]
     end
     puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
