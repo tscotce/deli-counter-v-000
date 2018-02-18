3a52f5be789a750fd94320be5a45820c50d7c666
Tatyana Scotce <tscotce@gmail.com>
2018-02-18 21:25:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,7 @@ def line(katz_deli)
   elsif katz_deli.length > 0 
     katz_deli.each_with_index do |name, index|
       helper_array << index+1
+      helper_array << name
     end
     helper_array.join()
     puts "The line is currently: #{index+1}. #{name}"  
