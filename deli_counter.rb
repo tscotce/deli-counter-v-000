38600fd8b867ac286634203091dc4198c9589872
Tatyana Scotce <tscotce@gmail.com>
2018-02-18 20:32:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ def take_a_number(katz_deli, name)
    puts "Welcome, #{name}. You are number 1 in line."
   elsif katz_deli.length > 0
     katz_deli.push(name)
-    number = katz_deli.index(name)-1
+    number = katz_deli.index(name)+1
     puts "Welcome, #{name}. You are number #{number} in line."
   end
 end
