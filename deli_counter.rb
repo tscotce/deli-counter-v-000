d63ce5ff9738798b8b1f8a68f91c0365556f38e7
Tatyana Scotce <tscotce@gmail.com>
2018-02-18 21:36:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,6 @@ def line(katz_deli)
     katz_deli.each_with_index do |name, index|
       line = "#{index+1}. #{name}"
       helper_array.push(line)
-      helper_array.join 
       #puts "The line is currently: #{helper_array}"
     end
     puts helper_array.join
