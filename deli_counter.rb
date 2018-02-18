2b5abd52193634706881c1d5cbaf279ee3d7964f
Tatyana Scotce <tscotce@gmail.com>
2018-02-18 20:07:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli) 
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  elsif katz_deli.length > 0 
+    puts 
+  end
+end
