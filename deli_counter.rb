7a12c8010453c67fbc046fef511fd9f62b9f6679
Tatyana Scotce <tscotce@gmail.com>
2018-02-18 21:34:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(katz_deli)
       line = "#{index+1}. #{name}"
       helper_array.push(line)
       helper_array.join 
-      puts "The line is currently: #{helper_array}"
+      #puts "The line is currently: #{helper_array}"
     end
   end
 end
